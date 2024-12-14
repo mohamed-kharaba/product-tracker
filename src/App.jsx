5f1ceee61defc86f4213d4ec2e276250b60983ff
Mohamed Kharaba <msk_h85@hotmail.com>
2024-12-14 11:41:15 +0300

    add filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 function App() {
+    const [selectCategory, setSelectCategory] = useState("");
+
     const [expenses, setExpenses] = useState([
         { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-        { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+        { id: 2, description: "bbb", amount: 10, category: "Groceries" },
         { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-        { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+        { id: 4, description: "ddd", amount: 10, category: "Entertainment" },
     ]);
+
+    const visibleExpenses = selectCategory ? expenses.filter((e) => e.category === selectCategory) : expenses;
 
     return (
         <>
-            <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
+            <div className="mb-3">
+                <ExpenseFilter onSelectCategory={(category) => setSelectCategory(category)} />
+            </div>
+            <ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))} />
         </>
     );
 }
---
+++ src/components/ExpenseFilter.jsx
@@ -0,0 +1,12 @@
+const ExpenseFilter = ({ onSelectCategory }) => {
+    return (
+        <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
+            <option value=""> All categories </option>
+            <option value="Groceries"> Groceries </option>
+            <option value="Utilities"> Utilities </option>
+            <option value="Entertainment"> Entertainment </option>
+        </select>
+    );
+};
+
+export default ExpenseFilter;
